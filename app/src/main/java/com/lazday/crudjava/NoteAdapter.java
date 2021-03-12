840e697fcb161a00f422d923ecdfc33270ea6e21
hiirsyad <mchirsyd.dev@gmail.com>
2021-03-12 11:54:24 +0700

    update adapter

--- app/src/main/java/com/lazday/crudjava/NoteAdapter.java
+++ app/src/main/java/com/lazday/crudjava/NoteAdapter.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.card.MaterialCardView;
-
 import java.util.List;
 
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
@@ -36,7 +34,7 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
         final NoteModel.Data data = dataList.get(position);
         viewHolder.textNote.setText( data.getNote() );
-        viewHolder.container.setOnClickListener(new View.OnClickListener() {
+        viewHolder.textNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 listener.onUpdate(data);
@@ -57,12 +55,10 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView textNote;
-        MaterialCardView container;
         ImageView imageDelete;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textNote = itemView.findViewById(R.id.text_note);
-            container = itemView.findViewById(R.id.container);
             imageDelete = itemView.findViewById(R.id.image_delete);
         }
     }
